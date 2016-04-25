375d5226ba4e30d90ae7adf5f4f2f2eb1943c8e8
ednodete <viktor_dimitrievski@hotmail.com>
2016-04-25 04:46:03 +0200

    add new style & functions

---
+++ js/script-for-styles.js
@@ -0,0 +1,28 @@
+//Ova e funkcija koja stava efekt na kopceto koga ke se klikne
+$(function () {
+    var btnClick, d, x, y;
+    $(".btn").click(function (e) {
+        if ($(this).find(".btn-over").length === 0) {
+            $(this).prepend("<span class='btn-over'></span>");
+        }
+
+        btnClick = $(this).find(".btn-over");
+        btnClick.removeClass("animation");
+
+        if (!btnClick.height() && !btnClick.width()) {
+            d = Math.max($(this).outerWidth(), $(this).outerHeight());
+            btnClick.css({
+                height: d,
+                width: d
+            });
+        }
+
+        x = e.pageX - $(this).offset().left - btnClick.width() / 2;
+        y = e.pageY - $(this).offset().top - btnClick.height() / 2;
+
+        btnClick.css({
+            top: y + 'px',
+            left: x + 'px'
+        }).addClass("animation");
+    });
+});
